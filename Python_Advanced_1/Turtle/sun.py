aeb1144b465491a49322f22130e69493644f7cdc
Stathis Zontos <stathis.zontos@gmail.com>
2025-10-18 11:48:50 +0300

    Finished sun project for Advanced 1

--- Python_Advanced_1/Turtle/sun.py
+++ Python_Advanced_1/Turtle/sun.py
@@ -20,5 +20,29 @@ for i in range(36):
     t.goto(0,100)
     t.right(10)
 
+t.penup()
+t.goto(50, 120)
+t.pendown()
+t.color("black")
+t.begin_fill()
+t.circle(10)
+t.end_fill()
 
+t.penup()
+t.goto(-50, 120)
+t.pendown()
+t.color("black")
+t.begin_fill()
+t.circle(10)
+t.end_fill()
+
+# Smile
+t.penup()
+t.goto(-50, 70)
+t.setheading(-60) # new
+t.pendown()
+t.width(4)
+t.circle(60, 120)
+
+t.hideturtle()
 turtle.done()
