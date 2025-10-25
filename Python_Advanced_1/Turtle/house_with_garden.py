c2bbd1b1e9abdbfa49169be2927a527bec66e4b8
Stathis Zontos <stathis.zontos@gmail.com>
2025-10-25 20:48:34 +0300

    Added OOP Folder

--- Python_Advanced_1/Turtle/house_with_garden.py
+++ Python_Advanced_1/Turtle/house_with_garden.py
@@ -7,7 +7,6 @@ screen.bgcolor("skyblue")
 # Grass
 t.penup()
 t.goto(-400, -200)
-
 t.color("green")
 t.begin_fill()
 t.right(90)
@@ -18,6 +17,34 @@ t.left(90)
 t.fd(200)
 t.left(90)
 t.fd(800)
+t.end_fill()
+
+# House
+t.setheading(0)
+t.penup()
+t.goto(-100, -200)
+t.pendown()
+t.color("saddlebrown")
+t.begin_fill()
+for i in range(2):
+    t.fd(200)
+    t.left(90)
+    t.fd(250)
+    t.left(90)
+t.end_fill()
+
+# Σκεπή
+t.penup()
+t.goto(-100, 50)
+t.pendown()
+t.setheading(0)
+t.color("red")
+t.begin_fill()
+t.fd(200)
+t.left(120)
+t.fd(200)
+t.left(120)
+t.fd(200)
 t.end_fill()
 
 t.hideturtle()
