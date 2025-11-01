761a4a11c2f15d8c19daa98fb1d789be31a3dfc9
Stathis Zontos <stathis.zontos@gmail.com>
2025-11-01 12:30:01 +0200

    Added class Dog

--- Python_Advanced_1/OOP/intro_to_oop.py
+++ Python_Advanced_1/OOP/intro_to_oop.py
@@ -29,4 +29,34 @@ class Car:
         self.model = model
         self.year = year
 
+    def describe_car(self):
+        """Return a string that describes our car"""
+        return f"I have a {self.make} model {self.model} from {self.year}"
+
+marilena_car = Car("Toyota", "Yaris", 2024)
+print(marilena_car.describe_car())
+
+class Dog:
+    """A simple attempt to represent a dog"""
+
+    def __init__(self, name, age, skin_color, breed="no breed"):
+        self.name = name
+        self.age = age
+        self.skin_color = skin_color
+        self.breed = breed
+
+    def describe_dog(self):
+        """Returns a string that describes my dog"""
+        return f"My dog is {self.name} and it is {self.age} years old. It has {self.skin_color} skin_color and it is a {self.breed}"
+
+    def sit(self):
+        """Tells the dog to sit"""
+        print(f"{self.name} sit down!")
+    
+    def roll_over(self):
+        """Tells the dog to roll over"""
+        print(f"{self.name} roll over`!")
+
+my_dog = Dog("Rico", 5, "black")
+print(my_dog.describe_dog())
 
